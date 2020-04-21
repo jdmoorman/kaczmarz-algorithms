1dccc1870e02898957e4fd525da0e7514c7eea0a
Jacob Moorman <jdmoorman@math.ucla.edu>
2020-04-21 08:10:13 -0700

    Rename selection.py to _selection_strategies.py

--- src/kaczmarz/_selection_strategies.py
+++ src/kaczmarz/_selection_strategies.py
@@ -17,6 +17,7 @@ class Cyclic(SelectionStrategy):
     """
 
     def __init__(self, A, *args, **kwargs):
+        super().__init__(A, *args, **kwargs)
         self.n_rows = A.shape[0]
         self.row_index = -1
 
@@ -35,10 +36,3 @@ class Cyclic(SelectionStrategy):
         """
         self.row_index = (1 + self.row_index) % self.n_rows
         return self.row_index
-
-
-strategies = {
-    attr_name: value
-    for attr_name, value in vars().items()
-    if not attr_name.startswith("_") and attr_name != "SelectionStrategy"
-}
