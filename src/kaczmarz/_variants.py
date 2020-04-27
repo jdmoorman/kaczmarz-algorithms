e6ff3d1ea98cd8efffe7208780707a7e9481cc3e
Jacob Moorman <jdmoorman@math.ucla.edu>
2020-04-27 12:57:13 -0700

    Improve all the docstrings.

--- src/kaczmarz/_variants.py
+++ src/kaczmarz/_variants.py
@@ -6,26 +6,29 @@ import kaczmarz
 class Cyclic(kaczmarz.Base):
     """Cycle through the equations of the system in order, repeatedly.
 
-    Notes
-    -----
-    This class inherits the parameters, methods, and properties of :class:`kaczmarz.Base`.
+    Note
+    ----
+    This class inherits the parameters, methods, and properties of
+    :class:`kaczmarz.Base`.
 
     Parameters
     ----------
-    args : tuple
+    base_args : tuple
         Positional arguments for :class:`kaczmarz.Base` constructor.
-    kwargs : dict
+    base_kwargs : dict
         Keyword arguments for :class:`kaczmarz.Base` constructor.
 
     References
     ----------
     1. Kaczmarz, S.: Angenäherte Auflösung von Systemen linearer Gleichungen.
-       *Bulletin International de l’Académie Polonaise des Sciences et des Lettres.
-       Classe des Sciences Mathématiques et Naturelles. Série A, Sciences Mathématiques* 35, 335–357 (1937)
+       *Bulletin International de l’Académie Polonaise
+       des Sciences et des Lettres.
+       Classe des Sciences Mathématiques et Naturelles.
+       Série A, Sciences Mathématiques* 35, 335–357 (1937)
     """
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, *base_args, **base_kwargs):
+        super().__init__(*base_args, **base_kwargs)
         self.n_rows = self._A.shape[0]
         self.row_index = -1
 
