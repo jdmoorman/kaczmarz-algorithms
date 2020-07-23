647e56da95c23911f972a4e07fc0f7430237e29b
Jacob Moorman <jdmoorman@math.ucla.edu>
2020-07-23 11:21:06 -0700

    Bump version: 0.6.0 → 0.7.0

--- setup.py
+++ setup.py
@@ -82,6 +82,6 @@ setup(
     url="https://github.com/jdmoorman/kaczmarz-algorithms",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.6.0",
+    version="0.7.0",
     zip_safe=False,
 )
