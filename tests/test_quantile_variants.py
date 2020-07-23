c1578ebca86219ae17ee018adeef2fa473b39d2d
Jacob Moorman <jdmoorman@math.ucla.edu>
2020-07-22 17:11:42 -0700

    linting.

--- tests/test_quantile_variants.py
+++ tests/test_quantile_variants.py
@@ -1,6 +1,7 @@
-import kaczmarz
 import numpy as np
 import pytest
+
+import kaczmarz
 
 np.random.seed(0)
 
@@ -17,10 +18,11 @@ for name in dir(kaczmarz):
         continue
     quantile_strategies.append(attr)
 
+
 def get_systems():
     examples = []
 
-    A = np.arange(1, 11).reshape(-1, 1) # [[1], [2], ...]
+    A = np.arange(1, 11).reshape(-1, 1)  # [[1], [2], ...]
     x = np.array([1])
     examples.append((A, x))
 
@@ -30,33 +32,33 @@ def get_systems():
 
     return examples
 
+
 class Sample100(kaczmarz.SampledQuantile):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, n_samples=10, **kwargs)
+
 
 class Window100(kaczmarz.WindowedQuantile):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, window_size=100, **kwargs)
 
+
 quantile_strategies.append(Sample100)
 quantile_strategies.append(Window100)
+
 
 @pytest.mark.parametrize("A,x_exact", get_systems())
 @pytest.mark.parametrize("QuantileStrategy", quantile_strategies)
 def test_corrupted_systems(A, x_exact, QuantileStrategy):
-    n_rows = A.shape[0]
     maxiter = 100  # 100 iterations should be plenty for nice systems.
     # TODO: Add a proper convergence criterion for corrupted systems.
-    row_norms = np.linalg.norm(A, axis=1)
     b = A @ x_exact
     b[0] += 100
     x_approx = QuantileStrategy.solve(A, b, maxiter=maxiter, quantile=0.7)
 
-
     residual = b - A @ x_approx
 
-    tol = 1e-5;
+    tol = 1e-5
 
     assert residual[0] > 99
     assert np.linalg.norm(residual[1:]) < tol
-    # assert np.testing.assert_allclose(x_approx, x_exact, rtol=10 * tol)
