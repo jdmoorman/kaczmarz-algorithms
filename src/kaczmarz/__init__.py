7adad904a3f3125569446af031c82f0fd2b02ba1
Jacob Moorman <jdmoorman@math.ucla.edu>
2020-07-30 11:12:58 -0700

    Bump version: 0.7.0 → 0.8.0

--- src/kaczmarz/__init__.py
+++ src/kaczmarz/__init__.py
@@ -2,7 +2,7 @@
 
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.7.0"
+__version__ = "0.8.0"
 
 __author__ = "Jacob Moorman"
 __email__ = "jacob@moorman.me"
